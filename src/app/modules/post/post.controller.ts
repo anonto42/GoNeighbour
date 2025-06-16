45ee830d11742e5660c0f3f575d18d3eaacb44e6
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-16 13:04:49 +0600

    create post controller added

---
+++ src/app/modules/post/post.controller.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../../shared/catchAsync';
+import { getMultipleFilesPath, getSingleFilePath } from '../../../shared/getFilePath';
+import sendResponse from '../../../shared/sendResponse';
+import { PostService } from './post.service';
+
+const createPost = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...userData } = req.body;
+    const user = req.user;
+
+    const images = getMultipleFilesPath(req.files,"image")
+
+    const modified_location = {
+        inText: userData.location,
+        geoFormet: {
+            type: "Point",
+            coordinates: [userData.lat, userData.lan]
+        }
+    };
+    userData.location = modified_location;
+
+    const data = {
+        images,
+        ...userData
+    }
+
+    const result = await PostService.createPost(user,data);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Successfully created the post',
+      data: result,
+    });
+  }
+);
+
+export const PostController = { 
+    createPost
+};
