6c09ad8fb67cefda04c72c1118d7da763753fb72
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-16 13:05:45 +0600

    post data type on the the creat fucntion

---
+++ src/types/post.ts
@@ -0,0 +1,11 @@
+
+export type postT = {
+    createdBy: string,
+    title: string,
+    description: string,
+    amount: number,
+    work_time: Date,
+    deadline: Date,
+    images: string[],
+    location: {}
+}
