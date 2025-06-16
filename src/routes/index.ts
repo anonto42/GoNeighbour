1cf7caacf97f4a6fbb353970460b1c0309d0b1c9
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-16 11:28:08 +0600

    post end poine added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { AuthRoutes } from '../app/modules/auth/auth.route';
 import { UserRoutes } from '../app/modules/user/user.route';
 import { AdminRouter } from '../app/modules/admin/admin.route';
+import { PostRouter } from '../app/modules/post/post.route';
 const router = express.Router();
 
 const apiRoutes = [
@@ -16,6 +17,10 @@ const apiRoutes = [
   {
     path: '/admin',
     route: AdminRouter
+  },
+  {
+    path: "/post",
+    route: PostRouter
   }
 ];
 
