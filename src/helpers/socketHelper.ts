e53b01c2e192a8c13d2312ddc98d82bc2e7c4637
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-21 09:53:41 +0600

    added map database for send the data

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -2,15 +2,23 @@ import colors from 'colors';
 import { Server } from 'socket.io';
 import { logger } from '../shared/logger';
 
+const connectedUsers = new Map<string, string>();
+
 const socket = (io: Server) => {
   io.on('connection', socket => {
     logger.info(colors.blue('A user connected'));
+
+    socket.on("register",(userID: string)=>{
+      connectedUsers.set(userID, socket.id);
+      logger.info(colors.cyan(`User ${userID} connected with socket ${socket.id}`));
+    });
 
     //disconnect
     socket.on('disconnect', () => {
       logger.info(colors.red('A user disconnect'));
     });
+
   });
 };
 
-export const socketHelper = { socket };
+export const socketHelper = { socket, connectedUsers};
