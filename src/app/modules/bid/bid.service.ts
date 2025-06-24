974d17c2e94bd9c22801eec31ff72ea81e1e3995
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-24 11:32:35 +0600

    send a bid

--- src/app/modules/bid/bid.service.ts
+++ src/app/modules/bid/bid.service.ts
@@ -4,27 +4,47 @@ import { JwtPayload } from 'jsonwebtoken';
 import { Bids } from '../../../types/bid';
 import { User } from '../user/user.model';
 import { Post } from '../post/post.model';
-
-//@ts-ignore
-const io = global.io; // Socket added for all live events
+import { NotificationModel } from '../notification/notification.model';
+import { socketHelper } from '../../../helpers/socketHelper';
+import mongoose from 'mongoose';
 
 const sendBid = async (
   payload: JwtPayload,
   data: Bids
 ) => {
   const { id } = payload;
-  await User.isValidUser(id);
+  const sender = await User.isValidUser(id);
+  
+  const objID = await new mongoose.Types.ObjectId(data.to);
+ 
+  const user = await User.findById(objID).lean().exec();
 
-  const task = await Post.findById(data.postID)
-  if (!task) {
+  const notification = await NotificationModel.create({
+    for: user,
+    from: sender._id,
+    title: `You got a bid from ${sender.name}`,
+    discription: `${data.reason}`,
+    content: data.amount
+  })
+  if (!notification) {
     throw new ApiError(
-      StatusCodes.BAD_REQUEST, 
-      "User doesn't exist!"
-    );
-  };
+      StatusCodes.NOT_ACCEPTABLE,
+      "Notification not created!"
+    )
+  }
 
+  const targetSocketId = socketHelper.connectedUsers.get(user!._id.toString());
+  
+  // @ts-ignore
+  const io = global.io;
 
-  return { };
+  if (targetSocketId) {
+
+    console.log(targetSocketId)
+    io.to(targetSocketId).emit(`socket:notification:${user?._id}`, notification);
+  }
+
+  return notification;
 };
 
 export const BidService = {
--- src/app/modules/bid/bid.validation.ts
+++ src/app/modules/bid/bid.validation.ts
@@ -2,8 +2,10 @@ import { z } from 'zod';
 
 const defaultZodSchema = z.object({
   body: z.object({
-    somthing: z.string({ required_error: '' })
-  }),
+    amount: z.number({required_error: "You must give the amount"}),
+    reason: z.string({required_error: "you must give the reson for your bid"}),
+    to: z.string({required_error: "You must give the id that you want to send the bid!"})
+}),
 });
 
 
