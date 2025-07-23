9594a9797eb18065a41823dcdce8073216d88232
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-23 11:58:27 +0600

    data type for the import

--- src/app/modules/validation/validation.interface.ts
+++ src/app/modules/validation/validation.interface.ts
@@ -1,9 +1,6 @@
-import { Model, Types } from 'mongoose';
 
-export type IValidation = {
 
-};
-
-export type ResetTokenModel = {
-  isArray(token: string): any;
-} & Model<IValidation>;
+export type imageData = {
+    FaceImage: string;
+    NIDImage: string;
+}
