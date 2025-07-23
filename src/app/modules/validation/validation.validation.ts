582b3634332e8ec18f5c4ec73505a442ec687b53
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-23 11:54:41 +0600

    updated

--- src/app/modules/validation/validation.validation.ts
+++ src/app/modules/validation/validation.validation.ts
@@ -4,7 +4,6 @@ const FaceValidationZodSchema = z.object({
   body: z.object({
     FaceImage: z.any({ required_error: 'FaceImage is required' }),
     NIDImage: z.any({ required_error: 'NIDImage is required' }),
-    userID: z.string({ required_error: 'UserID is required' }),
   }),
 });
 
