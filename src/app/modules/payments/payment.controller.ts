c924238b8c174e198bd55462cbf62b3c9fe7ab19
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-27 17:29:00 +0600

    clened the connection account cheack

--- src/app/modules/payments/payment.controller.ts
+++ src/app/modules/payments/payment.controller.ts
@@ -5,6 +5,7 @@ import { StatusCodes } from "http-status-codes";
 import { PaymentService, stripe } from "./payment.service";
 import { depositSuccessPage, renderPaymentSuccessPage } from "../../../shared/templates";
 import { User } from "../user/user.model";
+import ApiError from "../../../errors/ApiError";
 
 const createConnectionAccount = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
@@ -24,63 +25,100 @@ const createConnectionAccount = catchAsync(
     }
 );
 
-const successPageAccount = catchAsync(async (
-    req: Request, res: Response, next: NextFunction
-) => {
+// const successPageAccount = catchAsync(async (
+//     req: Request, res: Response, next: NextFunction
+// ) => {
     
-    const { id } = req.params;
-    const account = await stripe.accounts.update(id, {});
+//     const { id } = req.params;
+//     const account = await stripe.accounts.update(id, {});
    
-    if (
-      account?.requirements?.disabled_reason &&
-      account?.requirements?.disabled_reason.indexOf('rejected') > -1
-    ) {
-      return res.redirect(
-        `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
-      );
-    }
-    if (
-      account?.requirements?.disabled_reason &&
-      account?.requirements?.currently_due &&
-      account?.requirements?.currently_due?.length > 0
-    ) {
-      return res.redirect(
-        `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
-      );
-    }
-    if (!account.payouts_enabled) {
-      return res.redirect(
-        `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
-      );
-    }
-    if (!account.charges_enabled) {
-      return res.redirect(
-        `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
-      );
-    }
-    if (account?.requirements?.past_due) {
-        return res.redirect(`${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`);
-    }
-    if (
-      account?.requirements?.pending_verification &&
-      account?.requirements?.pending_verification?.length > 0
-    ) {
-      return res.redirect(`${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`);
-    }
+//     if (
+//       account?.requirements?.disabled_reason &&
+//       account?.requirements?.disabled_reason.indexOf('rejected') > -1
+//     ) {
+//       return res.redirect(
+//         `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
+//       );
+//     }
+//     if (
+//       account?.requirements?.disabled_reason &&
+//       account?.requirements?.currently_due &&
+//       account?.requirements?.currently_due?.length > 0
+//     ) {
+//       return res.redirect(
+//         `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
+//       );
+//     }
+//     if (!account.payouts_enabled) {
+//       return res.redirect(
+//         `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
+//       );
+//     }
+//     if (!account.charges_enabled) {
+//       return res.redirect(
+//         `${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`,
+//       );
+//     }
+//     if (account?.requirements?.past_due) {
+//         return res.redirect(`${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`);
+//     }
+//     if (
+//       account?.requirements?.pending_verification &&
+//       account?.requirements?.pending_verification?.length > 0
+//     ) {
+//       return res.redirect(`${req.protocol + '://' + req.get('host')}/api/v1/payment/refresh/${id}`);
+//     }
     
-    const user = await User.findOneAndUpdate({"paymentValidation.accountID": id},{
-        "paymentValidation.accountCreated": true
-    },{
-        new: true
-    })
+//     const user = await User.findOneAndUpdate({"paymentValidation.accountID": id},{
+//         "paymentValidation.accountCreated": true
+//     },{
+//         new: true
+//     })
 
-    if (!user) {
-        res.status(StatusCodes.NOT_FOUND).send("User was not founded!")
-        return;
-    }
+//     if (!user) {
+//         res.status(StatusCodes.NOT_FOUND).send("User was not founded!")
+//         return;
+//     }
 
-    return res.send(renderPaymentSuccessPage(user.name,user.email))
+//     return res.send(renderPaymentSuccessPage(user.name,user.email))
+// });
+
+const successPageAccount = catchAsync(async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  const account = await stripe.accounts.update(id, {});
+  if( !account ) throw new ApiError(StatusCodes.NOT_FOUND,"Payment creadintals not found!")
+
+  const shouldRedirect = (
+      account?.requirements?.disabled_reason?.includes('rejected') ||
+      (account?.requirements?.disabled_reason && (account?.requirements?.currently_due?.length ?? 0) > 0) ||
+      !account.payouts_enabled ||
+      !account.charges_enabled ||
+      account.requirements?.past_due ||
+      (account.requirements?.pending_verification?.length ?? 0 > 0)
+  );
+
+  if (shouldRedirect) {
+      return res.redirect(`${req.protocol}://${req.get('host')}/api/v1/payment/refresh/${id}`);
+  }
+
+  const user = await User.findOneAndUpdate(
+      { "paymentValidation.accountID": id },
+      { "paymentValidation.accountCreated": true },
+      { new: true }
+  );
+
+  if (!user) {
+      return res.status(StatusCodes.NOT_FOUND).send("User was not found!");
+  }
+
+  return res.send(renderPaymentSuccessPage(user.name, user.email));
 });
+
 
 const refreshAccount = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
@@ -140,11 +178,11 @@ const createWithdrawSession = catchAsync(
 );
 
 export const PaymentController = {
-    createCheckoutSession,
-    createConnectionAccount,
-    successDeposit,
-    refreshAccount,
-    successPageAccount,
-    createWithdrawSession
+  createCheckoutSession,
+  createConnectionAccount,
+  successDeposit,
+  refreshAccount,
+  successPageAccount,
+  createWithdrawSession
 };
     
